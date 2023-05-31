1e08174c988ad0e5ef99105c8500a178158bda3b
ADISIVAN C <adisivanc@gmail.com>
2023-05-31 20:59:57 +0530

    Tailwind Elements

--- react/delete/my-app/src/App.js
+++ react/delete/my-app/src/App.js
@@ -1,8 +1,6 @@
 function App() {
   return (
-    <div className='bg-red-900 w-screen h-screen'>
-
-    </div>
+      <div>Hello World</div>
   );
 }
 
--- react/delete/my-app/src/index.js
+++ react/delete/my-app/src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/*Tailwind Elements*/
+import { Carousel, initTE } from "tw-elements";
+initTE({ Carousel });
+/*Tailwind Elements*/
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
