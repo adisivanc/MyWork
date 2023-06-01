2a913e6ab6f596c1990a7e73f1e986cafc42bf9b
ADISIVAN C <adisivanc@gmail.com>
2023-06-01 20:21:08 +0530

    CRUD

--- react/delete/my-app/src/App.js
+++ react/delete/my-app/src/App.js
@@ -1,6 +1,14 @@
+import Header from "./Header"
+import Content from "./Content"
+import Footer from "./Footer"
+
 function App() {
   return (
-      <div>Hello World</div>
+      <div className="text-gray-950 dark:text-white dark:bg-zinc-950">
+        <Header/>
+        <Content/>
+        <Footer/>
+      </div>
   );
 }
 
