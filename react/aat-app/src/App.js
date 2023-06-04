02a6ce6b90aec9954ce25ecf739d3b9a9d08b45e
ADISIVAN C <adisivanc@gmail.com>
2023-06-04 18:10:22 +0530

    Installed typescript

--- react/aat-app/src/App.js
+++ react/aat-app/src/App.js
@@ -3,6 +3,7 @@ function App() {
   return (
     <>
       <p className="bg-yellow-900 p-10">App Component</p>
+      <button className="btn btn-primary">Bootstrap</button>
     </>
   );
 }
--- react/aat-app/src/index.js
+++ react/aat-app/src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/* ASN Bootstrap */
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
