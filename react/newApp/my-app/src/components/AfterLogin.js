7997d1e79db11c1bef8df7e2c4965af590cd00e9
ADISIVAN C <adisivanc@gmail.com>
2023-06-15 16:25:31 +0530

    MAP SET

--- react/newApp/my-app/src/components/AfterLogin.js
+++ react/newApp/my-app/src/components/AfterLogin.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 const AfterLogin = () => {
+
+
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        let timer = setTimeout(() => {
+            setCount((count) => count + 1);
+        }, 1000);
+
+        return () => clearTimeout(timer)
+    },[]);
+
     return (
         <>
-            After Login
+            <h3>I've rendered {count} times!</h3>;
         </>
     )
 }
 
 export default AfterLogin;
+
