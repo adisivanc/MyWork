436bd3de489d05ab61781605b0b79a52861ec56d
ADISIVAN C <adisivanc@gmail.com>
2023-06-21 21:53:58 +0530

    P

--- react/newApp/my-app/src/components/AfterLogin.js
+++ react/newApp/my-app/src/components/AfterLogin.js
@@ -1,22 +1,11 @@
 import React from "react";
-import { useState, useEffect } from "react";
 
 const AfterLogin = () => {
 
 
-    const [count, setCount] = useState(0);
-
-    useEffect(() => {
-        let timer = setTimeout(() => {
-            setCount((count) => count + 1);
-        }, 1000);
-
-        return () => clearTimeout(timer)
-    },[]);
-
     return (
         <>
-            <h3>I've rendered {count} times!</h3>;
+            <h3>I've rendered count times!</h3>;
         </>
     )
 }
--- react/newApp/my-app/src/components/BeforeLogin.js
+++ react/newApp/my-app/src/components/BeforeLogin.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const BeforeLogin = () => {
 
     const navigate = useNavigate();
+    const [userName,setUserName] = useState("");
+    const [password,setPassword] = useState("");
 
     const handleLogin = () => {
         navigate("/landing");
@@ -11,7 +14,24 @@ const BeforeLogin = () => {
 
     return (
         <>
-            <button type="button" className="btn btn-primary" onClick={handleLogin}>LOGIN</button>
+            <div className="loginForm">
+            <form className="form formVer">
+                <span>
+                    <h4>LOGO</h4>
+                </span>
+                <span>
+                    <label for="userName">Username</label>
+                    <input type="text" id="userName" className="form-input" value={userName} onChange={(e)=>setUserName(e.target.value)} />
+                </span>
+                <span>
+                    <label for="userName">Password</label>
+                    <input type="password" id="password" className="form-input" value={password} onChange={(e)=>setPassword(e.target.value)} />
+                </span>
+                <span>
+                    <button type="button" className="btn-primary" onClick={handleLogin}>LOGIN</button>
+                </span>
+            </form>
+            </div>
         </>
     )
 }
